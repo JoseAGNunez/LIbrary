69f9449ddc581ed91984769d67549a27154d8fb4
Jose Angel Garcia Nunez <joseagnunez@gmail.com>
2023-07-05 18:47:41 -0700

    Add myLibrary array and displayLibrary function to script.js, and add CSS reset to styles.css

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let myLibrary = [
+    {title: "Harry Potter", author: "Terf", pages: 0, read: false}, {title: "1984", author: "George Orwell", pages: 0, read: true}
+];
+
 function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -6,3 +10,23 @@ function Book (title, author, pages, read) {
     this.info = function(){
         return `${title} by ${author}, ${pages} pages, read: ${read}`;
     }
+};
+
+function displayLibrary(){
+    const body = document.body;
+    let myLibraryDiv = document.createElement('div');
+    myLibraryDiv.id = 'myLibraryDiv';
+    body.appendChild(myLibraryDiv);
+    myLibrary.forEach((book, index) => {
+        let bookDiv = document.createElement('div');
+        bookDiv.id = 'book' + (index);
+        myLibraryDiv.appendChild(bookDiv);
+        for (let key in book){
+            let bookInfoDiv = document.createElement('div');
+            bookDiv.appendChild(bookInfoDiv);
+            bookInfoDiv.textContent = `${key}: ${book[key]}`;
+        }
+    })
+};
+
+displayLibrary();
