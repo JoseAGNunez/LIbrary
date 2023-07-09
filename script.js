3f8f06edc8990c5621fe3b4abd84570c04f913a1
Jose Angel Garcia Nunez <joseagnunez@gmail.com>
2023-07-09 15:05:32 -0700

    Add book form functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const body = document.body;
+const newDiv = document.createElement('div');
+
 let myLibrary = [
     {title: "Harry Potter", author: "Terf", pages: 0, read: false}, {title: "1984", author: "George Orwell", pages: 0, read: true}
 ];
@@ -12,8 +15,15 @@ function Book (title, author, pages, read) {
     }
 };
 
+Book.prototype.readStatus = function() {
+    if (this.read === true) {
+        this.read = false;
+    } else {
+        this.read = true;
+    }
+}
+
 function displayLibrary(){
-    const body = document.body;
     let myLibraryDiv = document.createElement('div');
     myLibraryDiv.id = 'myLibraryDiv';
     body.appendChild(myLibraryDiv);
@@ -30,3 +40,49 @@ function displayLibrary(){
 };
 
 displayLibrary();
+
+function addBook(){
+    let hideLibrary = document.getElementById('myLibraryDiv');
+    let addBookButton = document.createElement('button');
+    body.appendChild(addBookButton);
+    addBookButton.textContent = 'Add Book';
+    addBookButton.addEventListener('click', function(){
+
+        hideLibrary.remove();
+        addBookButton.remove();
+
+        let addBookForm = document.createElement('form');
+        body.appendChild(addBookForm);
+
+        //Title
+        let titleLabel = document.createElement('label');
+        titleLabel.setAttribute('for','title');
+        titleLabel.textContent = 'Title';
+        addBookForm.appendChild(titleLabel);
+        
+        let titleInput = document.createElement('input');
+        addBookForm.appendChild(titleInput);
+        titleInput.setAttribute('type','text');
+        titleInput.setAttribute('name','title');
+        titleInput.id = 'title';
+
+        //Author
+        let authorLabel = document.createElement('label');
+        authorLabel.setAttribute('for','author');
+        authorLabel.textContent = 'Author';
+        addBookForm.appendChild(authorLabel);
+
+        let authorInput = document.createElement('input');
+        addBookForm.appendChild(authorInput);
+        authorInput.setAttribute('type','text');
+        authorInput.setAttribute('name','author');
+        authorInput.id = 'author';
+
+        // pages
+
+        let pagesLabel = document.createElement('label');
+
+    })
+}
+
+addBook();
